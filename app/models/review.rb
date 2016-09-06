333a07227e99a0f05b1a9ebae1f793e345da9e73
fidelscodes <fidelscodes@gmail.com>
2016-09-06 15:46:26 -0400

    add :trips association for User as guest

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ActiveRecord::Base
-  belongs_to :guest, :class_name => 'User' # foreign_key - guest_id
+  belongs_to :guest, class_name: 'User' # foreign_key - guest_id
   belongs_to :reservation # foreign_key - reservation_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ActiveRecord::Base
   # user has_many listings, we need to specify which foreign key to look for on
   # the listings table, otherwise, ActiveRecord is going to default to looking
   # for a user_id when we named it host_id
-  has_many :listings, :foreign_key => 'host_id'
+  # as a host:
+  has_many :listings, foreign_key: :host_id
   has_many :reservations, through: :listings
-  # has_many :reservations, as: :trips
-  # As a host has many listings, and many reservations through listings
 
+  # as a guest
+  has_many :trips, class_name: 'Reservation', foreign_key: :guest_id
+  # for this association, we need to tell AR which table :trips is referring to, and also specify which foreign_key to look for (:guest_id)
 
 end
