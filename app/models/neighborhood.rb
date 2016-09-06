403f2078d55d15b334c024cc4c86e2c453b7d774
ozPop <ozziepopovas@gmail.com>
2016-09-06 15:59:44 -0400

    add associations to all models

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,4 @@
 class Neighborhood < ActiveRecord::Base
+  belongs_to :city # fk :city_id
+  has_many :listings
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,5 @@
 class Reservation < ActiveRecord::Base
+  belongs_to :guest, class_name: "User" # fk :guest_id
+  belongs_to :listing # fk :listing_id
+  has_one :review
 end
