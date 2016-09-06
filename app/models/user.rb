bdbe3d250c039b84bd6c302a19ead91f50ca07bb
fidelscodes <fidelscodes@gmail.com>
2016-09-06 16:00:25 -0400

    add associations for user as a guest having many reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
   # user has_many listings, we need to specify which foreign key to look for on
   # the listings table, otherwise, ActiveRecord is going to default to looking
   # for a user_id when we named it host_id
+
   # as a host:
   has_many :listings, foreign_key: :host_id
   has_many :reservations, through: :listings
 
   # as a guest
+    # for this association, we need to tell AR which table :trips is referring to, and also specify which foreign_key to look for (:guest_id)
   has_many :trips, class_name: 'Reservation', foreign_key: :guest_id
-  # for this association, we need to tell AR which table :trips is referring to, and also specify which foreign_key to look for (:guest_id)
 
+  # We also need to specify that a guest has many reviews, so we need to tell it what foreign_key to look for. Otherwise, it defaults to the use_id foreign_key
+  has_many :reviews, foreign_key: :guest_id
 end
