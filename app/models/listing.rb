1bf2c22d4b3f0ae26fd28d408c55a60a4240984a
fidelscodes <fidelscodes@gmail.com>
2016-09-06 13:40:03 -0400

    update user and listing model to add associations between both

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,6 @@
 class Listing < ActiveRecord::Base
+  # ActiveRecord's belongs_to method is going to look for a table named "host",
+  # but that doesn't exist. Instead, we need to tell ActiveRecord which table
+  # host is referring to
+  belongs_to :host, :class_name => 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  
+  # user has_many listings, we need to specify which foreign key to look for on
+  # the listings table, otherwise, ActiveRecord is going to default to looking
+  # for a user_id when we named it host_id
+  has_many :listings, :foreign_key => 'host_id'
 end
